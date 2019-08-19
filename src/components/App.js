8409e98a61eb75774a61c6488354d942b3505e62
pettr-lee <peter@stagwell.tech>
2019-08-19 11:47:38 -0400

    adding new component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './Header/Header';
-import Footer from './Footer/Footer'
+import SelectDropdown from './SelectDropdown/SelectDropdown';
+import Footer from './Footer/Footer';
 
 function App() {
 	return (
 		<div>
 			<Header />
+			<SelectDropdown />
 			<Footer />
 		</div>
 	);
