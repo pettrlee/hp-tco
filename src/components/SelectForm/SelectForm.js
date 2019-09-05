bcbe2a2164066613828822c69272535c8b16ae88
pettr-lee <peter@stagwell.tech>
2019-09-05 18:11:12 -0400

    added global header and footers

--- src/components/SelectForm/SelectForm.js
+++ src/components/SelectForm/SelectForm.js
@@ -12,7 +12,7 @@ const SelectForm = (props) => {
     const Cpp = [2000, 2500, 3000, 3500, 4000, 4500, 5000, 5500, 6000, 6500, 7000];
 
     return (
-        <Form>
+        <Form id="tcoTool">
             <Container className="pagewide">
                 <Row className="title">
                     <h2>
