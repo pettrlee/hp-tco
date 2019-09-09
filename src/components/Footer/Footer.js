e702cdfd3139e3e17351b11e37badbb67f449a8a
pettr-lee <peter@stagwell.tech>
2019-09-09 15:58:09 -0400

    styling update adding gtm script

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,43 +7,41 @@ import Back from './back.png';
 
 const Footer = (props) => {
     return (
-        <footer className="fixed-bottom">
-            <Container>
-                {props.selectActive &&
-                    <Row>
-                        <Button
-                            variant="next"
-                            size="lg"
-                            disabled={props.FormCheck}
-                            onClick={props.ToggleView} >
-                            Next
-                        </Button>
-                    </Row>
-                }
+        <Container as="footer">
+            {props.selectActive &&
+                <Row>
+                    <Button
+                        variant="next"
+                        size="lg"
+                        disabled={props.FormCheck}
+                        onClick={props.ToggleView} >
+                        Next
+                    </Button>
+                </Row>
+            }
 
-                {props.resultsActive &&
-                    <Row>
-                        <Button
-                            variant="prev"
-                            size="lg"
-                            onClick={props.ToggleView} >
-                            <img
-                                width={8}
-                                height={14}
-                                src={Back}
-                                alt="Previous" />
-                            Previous
-                        </Button>
-                        <Button
-                            variant="pdf"
-                            size="lg">
-                            Get Your PDF
-                        </Button>
-                    </Row>
-                }
+            {props.resultsActive &&
+                <Row>
+                    <Button
+                        variant="prev"
+                        size="lg"
+                        onClick={props.ToggleView} >
+                        <img
+                            width={8}
+                            height={14}
+                            src={Back}
+                            alt="Previous" />
+                        Previous
+                    </Button>
+                    <Button
+                        variant="pdf"
+                        size="lg">
+                        Get Your PDF
+                    </Button>
+                </Row>
+            }
 
-            </Container>
-        </footer>
+        </Container>
     );
 }
 
