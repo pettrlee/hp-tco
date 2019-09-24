207e114d1877cf84d4de35d122cd34b4f62bc5de
pettr-lee <peter@stagwell.tech>
2019-09-24 16:10:08 -0400

    updated home to be purecomponent added gtm analytics for home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { PureComponent } from "react";
 import { Container, Row, Col, Button, Image } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import "./Home.scss";
@@ -6,41 +6,57 @@ import "./Home.scss";
 // IMAGES
 import Printer from "./Printer.png";
 
-const Home = (props) => {
-    return (
-        <div id="banner">
-            <Container>
-                <Row>
-                    <Col
-                        className="copy"
-                        sm={6}
-                        lg={{ span: 5, offset: 1 }} >
-                        <h1>What am I likely paying for color pages?</h1>
-                        <p>See how HP Pagewide delivers the lowest cost per color page.</p>
-                        <Button
-                            data-linkid="Calculate Your PageWide Savings"
-                            data-linkplacement="Home"
-                            onClick={props.handleDataLayerPush.bind(this)}
-                            as={Link}
-                            variant="calculate"
-                            to="/select-device" >
-                            Calculate Your PageWide Savings
-                        </Button>
-                    </Col>
-                    <Col
-                        xs={{ span: 8, offset: 2 }}
-                        sm={{ span: 6, offset: 0 }} >
-                        <Image
-                            fluid
-                            width={486}
-                            height={559}
-                            src={Printer}
-                            alt="Printer" />
-                    </Col>
-                </Row>
-            </Container>
-        </div>
-    );
-}
+export default class Home extends PureComponent {
 
-export default Home;
+    componentDidMount() {
+        window.scrollTo(0, 0)
+
+        // analytics data layer push
+        window.dataLayer.push({
+            event: "e_pageView",
+            pageNameL5: "A3 PageWide TCO Tool",
+            pageNameL6: "",
+            pageNameL7: "",
+            pageNameL8: "",
+            loginStatus: true,
+            pageBusinessUnit: ""
+        });
+    }
+
+    render() {
+        return (
+            <div id="banner">
+                <Container>
+                    <Row>
+                        <Col
+                            className="copy"
+                            sm={6}
+                            lg={{ span: 5, offset: 1 }} >
+                            <h1>What am I likely paying for color pages?</h1>
+                            <p>See how HP Pagewide delivers the lowest cost per color page.</p>
+                            <Button
+                                data-linkid="Calculate Your PageWide Savings"
+                                data-linkplacement="Home"
+                                onClick={this.props.handleDataLayerPush.bind(this)}
+                                as={Link}
+                                variant="calculate"
+                                to="/select-device" >
+                                Calculate Your PageWide Savings
+                        </Button>
+                        </Col>
+                        <Col
+                            xs={{ span: 8, offset: 2 }}
+                            sm={{ span: 6, offset: 0 }} >
+                            <Image
+                                fluid
+                                width={486}
+                                height={559}
+                                src={Printer}
+                                alt="Printer" />
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
+        );
+    }
+}
